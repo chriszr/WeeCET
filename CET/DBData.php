0e6c6476384938ebe107ea226afd450c2391410c
harrandall <yangzr975@gmail.com>
2015-03-21 11:29:09 +0800

    Add additional define for DB

--- CET/DBData.php
+++ CET/DBData.php
@@ -1,5 +1,13 @@
 <?php
 
+//Define DB Connection
+define('DB_HOST',SAE_MYSQL_HOST_M);
+define('DB_USER',SAE_MYSQL_USER);
+define('DB_PWD',SAE_MYSQL_PASS);
+define('DB_DBNAME',SAE_MYSQL_DB);
+define('DB_PROT',SAE_MYSQL_PORT);
+define('DB_CHARSET','utf8');
+define('DB_TYPE','mysql');
 
 class getDBData{
     
